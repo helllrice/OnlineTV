375d7cf916c8d05211e86f7d4dfac2e3abb19984
Vasiliy <63386773+helllrice@users.noreply.github.com>
2022-02-14 23:45:06 +0300

    register form

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,26 @@
-const btnRegister=document.getElementById("register-btn");function openModal(e){const t=document.createElement("div");t.classList.add("modal"),document.querySelector("header").append(t)}window.onload=function(){let e=document.getElementById("preload");e.style.display="none"},btnRegister.addEventListener("click",openModal);
+const btnRegister=document.getElementById("register-btn");function openModal(t){const e=document.createElement("div");e.classList.add("modal"),document.querySelector("header").append(e),e.innerHTML=`
+        <button class="close-btn">X</button>
+        <h2 class="register-text">Registration</h2>
+        <div class="form">
+            <div class="form__item">
+                <h2 class="register-form__text">Your E-mail</h2>
+                <input type="text">
+            </div>
+            <div class="form__item">
+                <h2 class="form__item-text">Your Login</h2>
+                <input type="text">
+            </div>
+            <div class="form__item">
+                <h2 class="form__item-text">Your Password</h2>
+                <input type="text">
+            </div>
+            <div class="form__item">
+                <h2 class="form__item-text">Сonfirm Password</h2>
+                <input type="text">
+            </div>
+            <div class="form-buttons">
+                <button class="register-create">Create Account</button>
+                <button class="register-enter">Sign In</button>
+            </div>
+        </div>
+   `}function closeModal(t){}window.onload=function(){let t=document.getElementById("preload");t.style.display="none"},btnRegister.addEventListener("click",openModal,{once:!0});
--- src/js/script.js
+++ src/js/script.js
@@ -5,12 +5,42 @@ window.onload = function () {
     preloader.style.display = 'none'
 }
 
-btnRegister.addEventListener('click', openModal)
+btnRegister.addEventListener('click', openModal, {once: true})
 
 function openModal(e) {
    const div = document.createElement('div')
    div.classList.add('modal')
    document.querySelector('header').append(div)
+   div.innerHTML = `
+        <button class="close-btn">X</button>
+        <h2 class="register-text">Registration</h2>
+        <div class="form">
+            <div class="form__item">
+                <h2 class="register-form__text">Your E-mail</h2>
+                <input type="text">
+            </div>
+            <div class="form__item">
+                <h2 class="form__item-text">Your Login</h2>
+                <input type="text">
+            </div>
+            <div class="form__item">
+                <h2 class="form__item-text">Your Password</h2>
+                <input type="text">
+            </div>
+            <div class="form__item">
+                <h2 class="form__item-text">Сonfirm Password</h2>
+                <input type="text">
+            </div>
+            <div class="form-buttons">
+                <button class="register-create">Create Account</button>
+                <button class="register-enter">Sign In</button>
+            </div>
+        </div>
+   `
+}
+
+function closeModal(e) {
+    
 }
 
 
