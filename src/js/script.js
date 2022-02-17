1e5bba79770558d0c9576692b1b954c273a223ee
Vasiliy <63386773+helllrice@users.noreply.github.com>
2022-02-17 20:12:57 +0300

    modal

--- src/js/script.js
+++ src/js/script.js
@@ -2,7 +2,8 @@ const btnRegister = document.getElementById('register-btn')
 const popup = document.getElementById('popup')
 const closeBtn = document.getElementById('close-btn')
 const singUp = document.getElementById('sing-up')
-const formItems = document.querySelectorAll('.form__item')
+const formItems = document.querySelectorAll('.form__item:nth-child(3n+1)')
+const createAccount = document.getElementById('create')
 
 window.onload = function () {
     let preloader = document.getElementById('preload')
@@ -12,6 +13,7 @@ window.onload = function () {
 btnRegister.addEventListener('click', openModal)
 closeBtn.addEventListener('click', closeModal)
 singUp.addEventListener('click', entrance)
+createAccount.addEventListener('click', create)
 
 function openModal(e) {
    popup.style.display = 'block'
@@ -22,8 +24,12 @@ function closeModal(e) {
 }
 
 function entrance (e) {
-    
+    for (let i = 0; i < formItems.length; i++) {
+        formItems[i].style.display = 'none'
+    }
 }
+
+
 
 
 
